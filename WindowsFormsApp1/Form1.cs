861c6015a61c3332022727a35b7e98fe05a82d23
MPRGDAW1920 <rodes20257@iesmarenostrum.com>
2022-03-21 18:17:05 +0100

    SUBO 1 CODIGO A 19.50

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textoTelegrama;
+            char tipoTelegrama;
+            int numPalabras;
+            double coste;
+            //Leo el telegrama
+            textoTelegrama = telegrama.Text;
+            //Obtengo el primer carácter de la cadena
+            tipoTelegrama = textoTelegrama[0];
+            //Obtengo el número de palabras que forma el telegrama
+            numPalabras = textoTelegrama.Length;
+            //Si el telegrama es ordinario
+            if (tipoTelegrama == 'O' || tipoTelegrama == 'o')
+                if (numPalabras <= 10)
+                    coste = 25;
+                else
+                    coste = 0.5 * numPalabras;
+else
+//Si el telegrama es urgente
+if (tipoTelegrama == 'U' || tipoTelegrama == 'u')
+                if (numPalabras <= 10)
+                    coste = 5;
+                else
+                    coste = 5 + 0.75 * (numPalabras - 10);
+            else
+                coste = 0;
+            precio.Text = coste.ToString() + " euros";
+        }
+    }
+    }
+
